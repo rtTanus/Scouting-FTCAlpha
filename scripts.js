31a6cae595cce2ef068fa5f6d7892faa1bf7b107
rt.Tanus <rafaeltanus2@gmail.com>
2025-03-09 19:25:25 -0300

    bonito

--- scripts.js
+++ scripts.js
@@ -14,14 +14,16 @@ function setClimbAuto(number){
     clearClimbAutoButtons()
     buttonClimbAuto = number;
     document.getElementById(buttonClimbAuto).classList.toggle("btn-primary");
-    console.log(buttonClimbAuto)
+    document.getElementById("AutoClimb").value = number;
+    console.log(document.getElementById("AutoClimb").value)
 }
 
 function setClimbTeleop(number){
     clearClimbTeleopButtons()
     buttonClimbTeleop = number;
     document.getElementById(buttonClimbTeleop).classList.toggle("btn-primary");
-    console.log(buttonClimbTeleop)
+    document.getElementById("TeleopClimb").value = number;
+    console.log(document.getElementById("TeleopClimb").value)
 }
     
 function getAllValues(){
@@ -60,7 +62,34 @@ function clearClimbTeleopButtons(){
     document.getElementById("buttonClimbTeleop3").classList.add("btn-outline-primary");
 }
 
-function sendToForms(){
+function sendToForms() {
+    let data = {
+        teamName: document.getElementById("teamName").value,
+        teamNumber: parseInt(document.getElementById('teamNumber').value),
+        roundNumber: parseInt(document.getElementById('roundNumber').value),
+        netAutoSample: parseInt(document.getElementById('netAutoSample').value),
+        lowAutoSample: parseInt(document.getElementById('lowAutoSample').value),
+        highAutoSample: parseInt(document.getElementById('highAutoSample').value),
+        lowAutoSpecimen: parseInt(document.getElementById('lowAutoSpecimen').value),
+        netTeleopSample: parseInt(document.getElementById('netTeleopSample').value),
+        lowTeleopSample: parseInt(document.getElementById('lowTeleopSample').value),
+        highTeleopSample: parseInt(document.getElementById('highTeleopSample').value),
+        lowTeleopSpecimen: parseInt(document.getElementById('lowTeleopSpecimen').value),
+        highTeleopSpecimen: parseInt(document.getElementById('highTeleopSpecimen').value),
+        buttonClimbAuto: document.getElementById("AutoClimb").value,
+        buttonClimbTeleop: document.getElementById("TeleopClimb").value
+    };
 
+    fetch("https://script.google.com/macros/s/AKfycbx-mNG6Lvw03MC_Cbv8yb4CyqZzlbW0GyuJjpt5qkKmXpN39Ln28OTNjQWWy3lmTtw2AA/exec", {
+        method: "POST",
+        mode: "no-cors",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    }).then(() => {
+        alert("Dados enviados com sucesso!");
+    }).catch(err => {
+        console.error("Erro ao enviar:", err);
+    });
 }
-
